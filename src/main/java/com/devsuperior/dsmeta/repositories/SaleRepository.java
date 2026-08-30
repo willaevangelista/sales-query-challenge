1a2a3527d9456ba1089a5d8d0c3fd114c21c1552
Willa Evangelista <willaevangelista.pro@gmail.com>
2026-08-30 18:26:20 -0300

    feat: add findSalesReport query to SaleRepository

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,29 @@
 package com.devsuperior.dsmeta.repositories;
 
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsmeta.entities.Sale;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+    @Query(value = "SELECT new com.devsuperior.dsmeta.dto.SaleReportDTO(s.id, s.date, s.amount, s.seller.name) "
+            + "FROM Sale s "
+            + "WHERE s.date BETWEEN :minDate AND :maxDate "
+            + "AND LOWER(s.seller.name) LIKE LOWER(CONCAT('%', :name, '%'))",
+            countQuery = "SELECT COUNT(s) "
+                    + "FROM Sale s "
+                    + "WHERE s.date BETWEEN :minDate AND :maxDate "
+                    + "AND LOWER(s.seller.name) LIKE LOWER(CONCAT('%', :name, '%'))")
+    Page<SaleReportDTO> findSalesReport(@Param("minDate") LocalDate minDate,
+                                        @Param("maxDate") LocalDate maxDate,
+                                        @Param("name") String name,
+                                        Pageable pageable);
 
 }
